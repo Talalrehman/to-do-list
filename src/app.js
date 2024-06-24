fc137ad0170e71dbf06076bb90dad3e4aa7dc23c
Talalrehman <m.talal9989@gmail.com>
2024-06-24 20:08:43 +0500

    Comment

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import Welcome from './componets/Welcome'
-import Todoo from './componets/Todo'
+import React from "react";
+import Welcome from "./componets/Welcome";
+import Todoo from "./componets/Todo";
 // import Todolist from './componets/Todolist'
 
 function app() {
   return (
     <div>
-        <Welcome/>
-        <Todoo/>
-        
+      <Welcome />
+      <Todoo />
+      {/* Comment */}
     </div>
-  )
+  );
 }
 
-export default app
+export default app;
